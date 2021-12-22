a9a42e19f5ce954ea5c0a3ae2064aa873d0020e5
Irish Moore <jasonamoore@frontier.com>
2021-12-21 23:35:51 -0500

    setup html and css and started js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// set the variable that will start counter
+var startButton = document.querySelector("#start-btn");
+
+// create event listener for the click
+const timeH = document.querySelector('#count');
+const timeSecond = 5;
+
+timeH.innerHTML = `00:${timeSecond}`;
+
+

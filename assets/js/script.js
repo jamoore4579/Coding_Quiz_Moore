6a2b8d76cacca9207ba7f59bb24162536d5926fb
Irish Moore <jasonamoore@frontier.com>
2021-12-22 16:04:58 -0500

    have first question showing up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,59 @@
 // set the variable that will start counter
-var startButton = document.querySelector("#start-btn");
-var timeSecond = questions.length *15;
+var quizTimeEl = document.getElementById("timer")
+var counter = document.getElementById("counter")
+var startButton = document.getElementById("start-btn")
+var questionEl = document.getElementById("question")
+var selectAns1 = document.getElementById("answer1")
+var selectAns2 = document.getElementById("answer2")
+var selectAns3 = document.getElementById("answer3")
+var selectAns4 = document.getElementById("answer4")
+var feedbackEl = document.getElementById("feedback")
+var title = document.getElementById("title")
+var questionNum = 0
+var timeRemain = 0
+var quizCounter = 0
 
 
-// create event listener for the click
-startButton.addEventListener("click", function(){
+function startQuiz() {
+    timeRemain = 75
+    counterEl();
+    startQuizEl();
+}
 
-    //timer within the function
-    const counterEl = document.querySelector("#count");
-    counterEl.innerHTML = `00:${timeSecond}`;
-    const counter = setInterval (()=>{
-        timeSecond--;
-        counterEl.innerHTML = `00:${timeSecond}`;
-        if(timeSecond <= 0 || timeSecond < 1){
-            clearInterval(counter);
-        }
-    },1000)
-});
+function counterEl() {
+    counter.innerHTML = (timeRemain)
+    quizCounter = setInterval(count, 1000);
+}
+
+function count() {
+    if (timeRemain !==0) {
+        timeRemain--
+        counter.innerHTML = (timeRemain)
+    } else {
+        clearInterval(quizCounter)
+        quizOver();
+    }
+    return;
+}
+
+function startQuizEl() {
+    document.querySelectorAll(".main").forEach(main => { main.style.display = "none" })
+    document.querySelectorAll(".quiz").forEach(quiz => {quiz.style.display = "initial"})
+    quiz(questionNum);
+} 
+
+function quiz() {
+    if (questionNum >= questions.length) {
+        quizOver();
+    } else {
+        questionEl.innerHTML = (questions[questionNum].title)
+        selectAns1.innerHTML = (questions[questionNum].choices[0])
+        selectAns2.innerHTML = (questions[questionNum].choices[1])
+        selectAns3.innerHTML = (questions[questionNum].choices[2])
+        selectAns4.innerHTML = (questions[questionNum].choices[3])
+    }
+}
+
+
+
 

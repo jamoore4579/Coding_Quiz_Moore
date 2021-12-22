d198075561510f0d7e04a9b034075afa50a5c12c
Irish Moore <jasonamoore@frontier.com>
2021-12-22 09:15:42 -0500

    added the counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
 // set the variable that will start counter
 var startButton = document.querySelector("#start-btn");
+var timeSecond = questions.length *15;
+
 
 // create event listener for the click
-const timeH = document.querySelector('#count');
-const timeSecond = 5;
+startButton.addEventListener("click", function(){
 
-timeH.innerHTML = `00:${timeSecond}`;
-
+    //timer within the function
+    const counterEl = document.querySelector("#count");
+    counterEl.innerHTML = `00:${timeSecond}`;
+    const counter = setInterval (()=>{
+        timeSecond--;
+        counterEl.innerHTML = `00:${timeSecond}`;
+        if(timeSecond <= 0 || timeSecond < 1){
+            clearInterval(counter);
+        }
+    },1000)
+});
 
